d16fe25a12023bdf9f81e4e50d03bcdfbdb0bcd4
harrySingh04 <minder91singh@gmail.com>
2017-10-13 16:53:42 -0700

    DFS and Dijkstra Implementation

--- cs4660/search/searches.py
+++ cs4660/search/searches.py
@@ -53,23 +53,23 @@ def bfs(graph, initial_node, dest_node):
     return list
 
 
-
-
-
-
-
-
-
-
-    pass
-
 def dfs(graph, initial_node, dest_node):
     """
     Depth First Search
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
-    pass
+    for neighbor_nodes in graph.neighbors(initial_node):
+        if neighbor_nodes == dest_node:
+            return [graph.get_edge(initial_node,dest_node)]
+        else:
+            edge_path = dfs(graph,neighbor_nodes,dest_node)
+            if edge_path != []:
+                list = [graph.get_edge(initial_node,neighbor_nodes)]
+                list.extend(edge_path)
+                return list
+
+    return []
 
 def dijkstra_search(graph, initial_node, dest_node):
     """
@@ -77,7 +77,52 @@ def dijkstra_search(graph, initial_node, dest_node):
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
-    pass
+    # dictionary key variable to store the distance between the two nodes
+    distance_node = {}
+    # list to store the list of nodes visited
+    visited_nodes = []
+    # Dictionar key variable to store the list of parents of node visited
+    parent_list = {}
+    Q = []
+    distance_node[initial_node] = 0
+    parent_list[initial_node] = None
+    Q.append((0,initial_node))
+    while(bool(Q)):
+        sorted(Q, key=lambda x: x[0])
+        min_node =[Q.pop(0)]
+        cur_node = min_node[0][1]
+        visited_nodes.append(cur_node)
+        for neighbor_nodes in graph.neighbors(cur_node):
+            if neighbor_nodes not in visited_nodes:
+                if neighbor_nodes not in distance_node:
+                    distance_node[neighbor_nodes] = distance_node[cur_node] + graph.distance_nodes(cur_node,neighbor_nodes)
+                    value = distance_node[neighbor_nodes]
+                    Q.append((value,neighbor_nodes))
+                    parent_list[neighbor_nodes] = cur_node
+                else:
+                    if distance_node[neighbor_nodes] > (distance_node[cur_node] + graph.distance_nodes(cur_node,neighbor_nodes)):
+                        distance_node[neighbor_nodes] = distance_node[cur_node] + graph.distance_nodes(cur_node,neighbor_nodes)
+                        value = distance_node[neighbor_nodes]
+                        Q.append((value,neighbor_nodes))
+                        parent_list[neighbor_nodes] = cur_node
+        if dest_node in visited_nodes:
+            break
+
+
+    list = []
+    start_node = dest_node
+    while(parent_list[start_node] is not None):
+        par_node = parent_list[start_node]
+        #print("parent node",par_node)
+        edge = graph.get_edge(par_node,start_node)
+        #print("edge",edge)
+        list.append(edge)
+        start_node = par_node
+
+
+    list.reverse()
+    return list
+
 
 def a_star_search(graph, initial_node, dest_node):
     """
